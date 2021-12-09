a9f46f8afd8e8ab75ac70f8a329c4881eda3a1c8
TonyBrb <anthonybarbagallo95@gmail.com>
2021-12-09 16:27:27 +0100

    bundle 02

--- bundle-02/main.js
+++ bundle-02/main.js
@@ -12,33 +12,53 @@
 
 // ESERCIZIO 1
 function checkAge() {
-    const myAge = 32;
-    const message = '';
+    const myAge = 34;
+    let message = '';
 
     if (myAge < 18) {
         message = `Sei troppo giovane! Hai ${myAge} anni!`;
     } else {
         message = 'Hai più di 18 anni!';
     }
+    console.log(message);
 }
 checkAge();
+
+/*
+1- Dovrebbe dire all'utente se ha più o meno di 18 anni
+2- Si, a riga 16 message dovrebbe essere una variabile let e non una costante dato che successivamente viene modificata 
+3- No
+*/
+
 
 // ESERCIZIO 2
 function printColorsNumber() {
     const colors = ['blue', 'red', 'yellow', 'green', 'black'];
-    console.log(`Nella mia palette ci sono ${colors.lenght} colori!`);
+    console.log(`Nella mia palette ci sono ${colors.length} colori!`);
 }
 printColorsNumber();
+
+/*
+1- Dovrebbe dire quanti colori ci sono nell'array colors
+2- Si, a riga 37 lenght è scritto male, dovrebbe essere length
+3- No
+*/
 
 
 // ESERCIZIO 3
 function addNumbers() {
-    const userNumber = prompt('Inserisci un numero');
+    const userNumber = parseInt(prompt('Inserisci un numero'));
     const total = userNumber + 12;
 
     console.log(`Il risultato finale è ${total}`);
 }
 addNumbers();
+
+/*
+1- Dovrebbe aggiungere al numero inserito dall'utente 12 e  mostrarlo in console
+2- No
+3- Si a riga 50 il prompt dovrebbe stare all'interno di un parseInt
+*/
 
 
 // ESERCIZIO 4
@@ -49,7 +69,7 @@ function checkAccess() {
     let grantAccess = 'false';
 
     if (addresses.includes(userEmail)) {
-        grantAccess = 'true';
+        grantAccess = true;
     }
 
     if (grantAccess === true) {
@@ -59,6 +79,12 @@ function checkAccess() {
     }
 }
 checkAccess();
+
+/*
+1- Dovrebbe far accedere l'utente se la sua e-mail è presente nell'array addresses
+2- Si a riga 72 true non va tra apici
+3- No
+*/
 
 
 // ESERCIZIO 5 (suggerimento: c'è un solo errore)
@@ -75,12 +101,14 @@ function checkAccessImproved() {
       if (userEmail.length > 5) {
             
         if (email === userEmail) {
-        grantAccess = 'true';            
+        grantAccess = true;            
             
       }
         
     }
 
+    
+    }
     if (grantAccess) {
         console.log('Accesso consentito!');
     } else {
@@ -89,31 +117,8 @@ function checkAccessImproved() {
 }
 checkAccessImproved();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/*
+1- Dovrebbe far accedere l'utente se la sua e-mail è presente nell'array addresses
+2- Si a riga 104 true non va tra apici, a riga 116 va chiusa la graffa della funzione
+3- Si gli if da riga 110 a riga 114 vanno fuori dal for
+*/
